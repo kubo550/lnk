7b29d1ceae34358f4e091bc02541364edfc92ed1
= <qwercy142@gmail.com>
2024-12-04 23:45:38 +0100

    add url input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+'use client';
 import Image from "next/image";
+import {useState} from "react";
 
 export default function Home() {
+  const [url, setUrl] = useState('');
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -13,32 +17,26 @@ export default function Home() {
           priority
         />
 
-        test 1
+        Easiest way to shorten your URL. Get super short URL in few seconds.
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+        <div className="flex gap-4 items-center flex-col sm:flex-row w-full">
+
+          <input
+            className="w-full p-2 text-sm bg-gray-100 dark:bg-gray-800 dark:text-gray-100 rounded-md"
+            placeholder="Enter URL"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+            <button
+                className="p-2 text-sm font-semibold text-white bg-blue-600 rounded-md"
+                onClick={() => {
+                    console.log('URL:', url);
+                }}
+            >
+                Shorten
+            </button>
+
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
